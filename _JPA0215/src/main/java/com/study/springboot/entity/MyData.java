633f00897b6cbc5f84ecd6295a22c60f471880db
thxb5 <kjhcyz@naver.com>
2023-02-15 18:37:18 +0900

    0215

---
+++ _JPA0215/src/main/java/com/study/springboot/entity/MyData.java
@@ -0,0 +1,24 @@
+package com.study.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MyData {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String memo;
+}
